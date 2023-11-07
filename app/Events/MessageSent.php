10a6716d7047e7566eda3c1e676ea9db77728eaf
nakamukun <gotmonkey1227@gmail.com>
2023-11-07 18:55:19 +0900

    修正

--- app/Events/MessageSent.php
+++ app/Events/MessageSent.php
@@ -33,6 +33,6 @@ class MessageSent implements ShouldBroadcast
      */
     public function broadcastOn()
     {
-        return new PrivateChannel('chatroom');
+        return new hannel('chatroom');
     }
 }
